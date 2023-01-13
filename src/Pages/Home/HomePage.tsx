b9a4459038f25f3799d1c9d53b190c3256b03216
Thien Phuc Khuc <khucthienphuc@gmail.com>
2023-01-13 22:36:32 +0700

    ci: add filter for movie list at HomePage

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { useQuery } from '@tanstack/react-query';
 
 // import local services
@@ -6,6 +7,38 @@ import MOVIE_SERV from '../../core/services/movieServ';
 // import local components
 import MoviesList from './MoviesList';
 import InnerSpinner from '../../core/Components/Spinners/InnerSpinner';
+
+// import local constants
+import { TIME_FORMAT_CONST } from '../../core/constants/time.const';
+
+// import ANTD Components
+import { Button, DatePicker, Form, Input } from 'antd';
+const { RangePicker } = DatePicker;
+const dateFormat = TIME_FORMAT_CONST.DAY_MONTH_YEAR_FORMAT;
+
+// ANTD Config
+const formConfig = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+  initialValues: {
+    tenPhim: '',
+    ngayKhoiChieuPicker: [
+      dayjs(Date.now()),
+      dayjs(Date.now() + 6 * 30 * 24 * 60 * 60 * 1000),
+    ],
+  },
+};
+const rangeConfig = {
+  rules: [
+    { type: 'array' as const, required: true, message: 'Please select time!' },
+  ],
+};
 
 const HomePage = () => {
   const {
@@ -17,14 +50,52 @@ const HomePage = () => {
     cacheTime: 3600000,
   });
 
+  const onFinish = (fieldsValue: any) => {
+    // Should format date value before submit.
+    console.log('Received values of form: ', fieldsValue);
+    const rangeValue = fieldsValue['ngayKhoiChieuPicker'];
+    const fromDate = rangeValue[0].format('YYYY-MM-DD');
+    const toDate = rangeValue[1].format('YYYY-MM-DD');
+    const value = { tenPhim: fieldsValue.tenPhim, fromDate, toDate };
+    console.log('Output values of form: ', value);
+  };
+  const renderMovieFilter = () => (
+    <Form name="movie_filter_control" {...formConfig} onFinish={onFinish}>
+      <Form.Item name="tenPhim" label="Tên phim">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="ngayKhoiChieuPicker"
+        label="Ngày khỏi chiếu"
+        {...rangeConfig}
+      >
+        <RangePicker format={dateFormat} />
+      </Form.Item>
+      <Form.Item
+        wrapperCol={{
+          xs: { span: 24, offset: 0 },
+          sm: { span: 16, offset: 8 },
+        }}
+      >
+        <Button danger type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+
   return (
     <>
       <div className="container xl:max-w-screen-xl mx-auto">
         <h3 className="mt-10 font-semibold text-2xl">DANH SÁCH PHIM</h3>
+
         {isLoading || isFetching ? (
           <InnerSpinner />
         ) : (
-          <MoviesList moviesList={moviesList} />
+          <>
+            {renderMovieFilter()}
+            <MoviesList moviesList={moviesList} />
+          </>
         )}
       </div>
     </>
--- src/core/constants/time.const.ts
+++ src/core/constants/time.const.ts
@@ -1,7 +1,8 @@
 /* time format const */
 const TIME_FORMAT_CONST = {
-  FULL_TIME_MOVIE_FORMAT: "DD-MM-YYYY ~ hh:mm",
-  ONLY_YEAR_FORMAT: "YYYY",
+  DAY_MONTH_YEAR_FORMAT: 'DD-MM-YYYY',
+  FULL_TIME_MOVIE_FORMAT: 'DD-MM-YYYY ~ hh:mm',
+  ONLY_YEAR_FORMAT: 'YYYY',
 };
 
 export { TIME_FORMAT_CONST };
